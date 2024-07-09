0128bd6471653adbcfdbe3035fde21d2281e96b4
coltiq <dev@coltiq.com>
2024-07-09 18:32:42 -0500

    Add: Health & Err Handlers

---
+++ handlerReadiness.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	type response struct {
+		Status string `json:"status"`
+	}
+
+	respondWithJSON(w, http.StatusOK, response{
+		Status: "ok",
+	})
+}
+
+func handlerErr(w http.ResponseWriter, r *http.Request) {
+	respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+}
