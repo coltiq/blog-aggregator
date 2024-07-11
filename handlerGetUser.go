1e9b2bcb4b4d6a24011cdedc554c06fcc8dfb09a
coltiq <dev@coltiq.com>
2024-07-11 18:48:30 -0500

    Feat: Get User

--- handlerGetUser.go
+++ handlerGetUser.go
@@ -1,7 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/coltiq/blog-aggregator/internal/auth"
+)
 
 func (cfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(&r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, fmt.Sprintf("%s", err))
+		return
+	}
 
+	user, err := cfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get user")
+		return
+	}
+
+	respondWithJSON(w, http.StatusAccepted, databaseUserToUser(user))
 }
